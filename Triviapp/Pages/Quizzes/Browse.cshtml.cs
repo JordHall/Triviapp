b068a55163615725491ca8b5e5f1afe10bf5276f
Jordan Hall <32490517+JordHall@users.noreply.github.com>
2020-03-25 21:03:57 +0000

    Added List and Create scaffolded views for Quizzes

---
+++ Triviapp/Pages/Quizzes/Browse.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Triviapp.Data;
+using Triviapp.Models;
+
+namespace Triviapp
+{
+    public class BrowseModel : PageModel
+    {
+        private readonly Triviapp.Data.TriviappContext _context;
+
+        public BrowseModel(Triviapp.Data.TriviappContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Quiz> Quiz { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Quiz = await _context.Quizzes.ToListAsync();
+        }
+    }
+}
---
+++ Triviapp/Pages/Quizzes/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Triviapp.Data;
+using Triviapp.Models;
+
+namespace Triviapp
+{
+    public class CreateModel : PageModel
+    {
+        private readonly Triviapp.Data.TriviappContext _context;
+
+        public CreateModel(Triviapp.Data.TriviappContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Quiz Quiz { get; set; }
+
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Quizzes.Add(Quiz);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
