5a7062a354a0748ff48ae53168bcec1d73a71b67
Jordan Hall <32490517+JordHall@users.noreply.github.com>
2020-04-06 00:55:07 +0100

    Created scaffolded Play view, and implemented some initial logic

---
+++ Triviapp/Pages/Quizzes/Play.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Triviapp.Data;
+using Triviapp.Models;
+
+namespace Triviapp
+{
+    public class PlayModel : PageModel
+    {
+        private readonly Triviapp.Data.TriviappContext _context;
+
+        public PlayModel(Triviapp.Data.TriviappContext context)
+        {
+            _context = context;
+        }
+
+        public Quiz Quiz { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Quiz = await _context.Quizzes.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Quiz == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
