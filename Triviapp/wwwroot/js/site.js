b78c68912836514b423a434724e2f54b753b1310
Jordan Hall <32490517+JordHall@users.noreply.github.com>
2020-04-09 21:08:21 +0100

    Added restart functionality and scoring system to Quiz view

--- Triviapp/wwwroot/js/site.js
+++ Triviapp/wwwroot/js/site.js
@@ -1,30 +1,96 @@
-﻿var startButton = document.getElementById("start-btn")
-var questionContainer = document.getElementById("question-container")
-var questionElment = document.getElementById("question")
-var answerButtonsElment = document.getElementById("answer-buttons")
-let  shuffledQuestions, currentQuestionIndex
+﻿const startButton = document.getElementById("start-btn") //START BUTTON
+const nextButton = document.getElementById("next-btn") //NEXT BUTTON
+const questionContainer = document.getElementById("question-container") //QUESTION CONTAINER
+const questionElment = document.getElementById("question") //QUESTION TITLE
+const answerButtonsElment = document.getElementById("answer-buttons") //ANSWER BUTTONS
+const scoreSpan = document.getElementById("scoreSpan") // SCORE ELEMENT
+let shuffledQuestions, currentQuestionIndex
+let score = 0
 
+//Start Quiz on click
 startButton.addEventListener("click", startQuiz)
+nextButton.addEventListener("click", () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
 
-
-
-
+//QUIZ START
 function startQuiz() {
+    score = 0
+    updateScore()
     startButton.classList.add("hide")
-    shuffledQuestions = questionArray.sort(() => Math.random() - .5)
+    shuffledQuestions = questionArray.sort(() => Math.random() - .5) //RANDOMLY SORT
     currentQuestionIndex = 0
     questionContainer.classList.remove("hide")
     setNextQuestion()
 }
 
+//GET NEXT QUESTION
 function setNextQuestion() {
+    reset()
     showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
+//DISPLAY QUESTION & ANSWERS
 function showQuestion(question) {
     questionElment.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement("button")
+        button.innerText = answer.text
+        button.classList.add("btn-answer")
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", selectAnswer)
+        answerButtonsElment.appendChild(button)
+    })
 }
 
-function selectAnswer() {
-    
+//RESET BUTTONS
+function reset() {
+    nextButton.classList.add("hide")
+    while (answerButtonsElment.firstChild) {
+        answerButtonsElment.removeChild(answerButtonsElment.firstChild)
+    }
+}
+
+//SELECT ANSWER
+function selectAnswer(a) {
+    const selectedButton = a.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElment.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (correct) {
+        score++
+        updateScore()
+    }
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove("hide")
+    } else {
+        startButton.innerText = "Restart"
+        startButton.classList.remove("hide")
+    }
+}
+
+//SET ANSWER CLASSES
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add("correct")
+    } else {
+        element.classList.add("wrong")
+    }
+}
+
+//REMOVE ANSWER CLASSES
+function clearStatusClass(element) {
+    element.classList.remove("correct")
+    element.classList.remove("wrong")
+}
+
+//UPDATE SCORE
+function updateScore() {
+    scoreSpan.innerText = "Score: " + score.toString()
 }
