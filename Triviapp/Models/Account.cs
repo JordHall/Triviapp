6b4aebcbeacba7ed3516f3cbcc8e7a04e9d10d7f
Jordan Hall <32490517+JordHall@users.noreply.github.com>
2020-04-14 22:23:16 +0100

    Created Account class model

---
+++ Triviapp/Models/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Triviapp.Models
+{
+    public class Account
+    {
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public IList<Quiz> Quizzes { get; set; }
+    }
+}
