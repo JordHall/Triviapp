8167fc00c95e583234a15007a35c2a0503bdae3f
Jordan Hall <32490517+JordHall@users.noreply.github.com>
2020-04-15 20:30:09 +0100

    Added Login page

---
+++ Triviapp/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Triviapp.Models;
+
+namespace Triviapp
+{
+    public class LoginModel : PageModel
+    {
+        private readonly Triviapp.Data.TriviappContext _context;
+
+        public LoginModel(Triviapp.Data.TriviappContext context)
+        {
+            _context = context;
+        }
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Account Account { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Accounts.Add(Account);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("/Quizzes/Browse");
+        }
+    }
+}
