116a0a98f129f1b10369f4c1cf56ee987ea785af
Jordan Hall <32490517+JordHall@users.noreply.github.com>
2020-04-16 18:20:31 +0100

    Added Navbar links for Accounts and added a Profile page

---
+++ Triviapp/Pages/Account/Profile.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Triviapp.Models;
+
+namespace Triviapp
+{
+    public class ProfileModel : PageModel
+    {
+        private readonly Triviapp.Data.TriviappContext _context;
+
+        public ProfileModel(Triviapp.Data.TriviappContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Account Account { get; set; }
+        public void OnGet()
+        {
+            var Username = HttpContext.Session.GetString("username");
+            Account = _context.Accounts.SingleOrDefault(a => a.Username.Equals(Username));
+        }
+
+        public IActionResult OnPost()
+        {
+            _context.Entry(Account).State = EntityState.Modified;
+            _context.SaveChanges();
+            return RedirectToPage("Profile");
+        }
+    }
+}
