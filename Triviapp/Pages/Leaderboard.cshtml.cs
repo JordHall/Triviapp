75fb268392ab4b9210de9bc3bf8eb116aff254c2
Jordan Hall <32490517+JordHall@users.noreply.github.com>
2020-04-20 22:46:22 +0100

    Added Leaderboard Page

---
+++ Triviapp/Pages/Leaderboard.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Triviapp.Data;
+using Triviapp.Models;
+
+namespace Triviapp
+{
+    public class LeaderboardModel : PageModel
+    {
+        private readonly Triviapp.Data.TriviappContext _context;
+
+        public LeaderboardModel(Triviapp.Data.TriviappContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Account> Account { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Account = await _context.Accounts.OrderByDescending(a => a.Score).ToListAsync();
+        }
+    }
+}
