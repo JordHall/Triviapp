c1c26baa36eb9d3d833123c8a12b766955cabc73
Jordan Hall <32490517+JordHall@users.noreply.github.com>
2020-04-20 18:10:33 +0100

    Added Edit Quiz pages

---
+++ Triviapp/Pages/Quizzes/Edit.cshtml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Triviapp.Data;
+using Triviapp.Models;
+
+namespace Triviapp
+{
+    public class EditModel : PageModel
+    {
+        private readonly Triviapp.Data.TriviappContext _context;
+
+        public EditModel(Triviapp.Data.TriviappContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Quiz Quiz { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Quiz = await _context.Quizzes
+                .Include(q => q.Account).FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Quiz == null)
+            {
+                return NotFound();
+            }
+           ViewData["AccountID"] = new SelectList(_context.Accounts, "ID", "Username");
+            return Page();
+        }
+
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Quiz).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!QuizExists(Quiz.ID))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("Browse");
+        }
+
+        private bool QuizExists(int id)
+        {
+            return _context.Quizzes.Any(e => e.ID == id);
+        }
+    }
+}
