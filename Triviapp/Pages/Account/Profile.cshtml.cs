e026e56da508a593f88663892bdda864bb7722b1
Jordan Hall <32490517+JordHall@users.noreply.github.com>
2020-04-23 19:14:05 +0100

    Improved Profile to include users quizzes

--- Triviapp/Pages/Account/Profile.cshtml.cs
+++ Triviapp/Pages/Account/Profile.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using Triviapp.Models;
 
 namespace Triviapp
@@ -16,12 +17,24 @@ namespace Triviapp
         {
             _context = context;
         }
-
         [BindProperty]
         public Account Account { get; set; }
-        public void OnGet()
+        public async Task<IActionResult> OnGetAsync()
         {
-            Account = _context.Accounts.SingleOrDefault(a => a.Username.Equals(HttpContext.User.Identity.Name)); //GET ACCOUNT FROM DATABASE
+            try
+            {
+                Account = await _context.Accounts.FirstOrDefaultAsync(a => a.Username.Equals(HttpContext.User.Identity.Name)); //GET ACCOUNT FROM DATABASE
+                if (Account == null)
+                {
+                    return NotFound();
+                }
+                Account.Quizzes = await _context.Quizzes.Where(q => q.AccountID == Account.ID).ToListAsync(); //GET ACCOUNTS QUIZZES
+                return Page();
+            }
+            catch
+            {
+                return NotFound();
+            }
         }
     }
 }
